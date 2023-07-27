2b5385f0194bcd3969981a5c68c254bd4c5ef2ce
Du Poteaux <rolanddebotho19@gmail.com>
2023-07-27 12:21:37 +0100

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,69 @@
+  <!-- ======= Footer ======= -->
+  <section class="section-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">RapidAppart</h3>
+            </div>
+            <div class="w-body-a">
+              <p class="w-text-a color-text-a">
+                Enim minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat duis
+                sed aute irure.
+              </p>
+            </div>
+            <div class="w-footer-a">
+              <ul class="list-unstyled">
+                <li class="color-a">
+                  <span class="color-text-a">Phone .</span> rolandddebotho19@gmail.com
+                </li>
+                <li class="color-a">
+                  <span class="color-text-a">Email .</span> +237 699112031
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 section-md-t3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand"></h3>
+            </div>
+         
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">Newsletter</h3>
+            </div>
+            <div class="w-body-a">
+              <p class="w-text-a color-text-a">
+                Enim minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip exea commodo consequat duis
+                sed aute irure.
+              </p><br><br>
+
+            <p class="intro-subtitle intro-price">
+              <a href="#"><span class="price-a" style="color: #000;">Notre autre site web</span></a>
+            </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="copyright-footer">
+            <p class="copyright color-text-a">
+              <span class="color-a">2023</span> &copy; Tous droits réservés.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
---
+++ utilisateur/rapidappart/src/PriceFormat.php
@@ -0,0 +1,17 @@
+<?php
+
+    class PriceFormat{
+
+
+
+        public static function price(float $name, string $sigle = "F CFA"): string
+
+        {
+
+            return number_format($name, '2', '.',' '). ' ' .$sigle;
+
+        }
+
+
+
+    }
